e8627e1bbcada1f142f8e29ee0720a192e3ae86e
CelestialQwert <celestialqwert@gmail.com>
2024-08-21 02:03:06 -0400

    Prototype link program

---
+++ link_test.py
@@ -0,0 +1,215 @@
+import time
+import rp2
+import utime
+from micropython import const
+from machine import Pin
+from ulab import numpy as np
+
+
+STATE_IDLE = const(0)
+STATE_MAGICBYTES_PARTIAL = const(1)
+STATE_HEADER = const(2)
+STATE_PAYLOAD = const(3)
+STATE_CHECKSUM = const(4)
+STATE_RESPONSE_READY = const(5)
+STATE_RESPONSE_PARTIAL = const(6)
+STATE_TIMEOUT = const(7)
+
+COMMAND_INIT = const(1)
+COMMAND_PRINT = const(2)
+COMMAND_DATA = const(4)
+COMMAND_BREAK = const(8)
+COMMAND_STATUS = const(0xF)
+
+
+@rp2.asm_pio(
+    in_shiftdir=rp2.PIO.SHIFT_LEFT,
+    out_shiftdir=rp2.PIO.SHIFT_LEFT,
+    set_init=rp2.PIO.OUT_HIGH,
+    out_init=rp2.PIO.OUT_LOW,
+    sideset_init=rp2.PIO.OUT_LOW
+)
+def gb_link(): 
+    wrap_target() 
+    set(x, 6)             # set loop to run 6 + 1 times
+    wait(0, gpio, 5)      # wait for falling edge
+    set(pins, 0)          # turn off LED
+    pull(noblock)         # pull value from TX FIFO to OSR
+    out(null, 24)         # shift left by 24
+    out(pins, 1)          # out the MSB bit in OSR to GB
+    wait(1, gpio, 5)[2]   # wait for rising edge
+    label("bitloop")      
+    in_(pins, 1)          # input bit from GB to ISR
+    wait(0, gpio, 5)[2]   # wait for falling edge
+    out(pins, 1)          # output bit from OSR to GB
+    wait(1, gpio, 5)[1]   # wait for rising edge
+    jmp(x_dec, "bitloop") # loop through the rest of the bits
+    in_(pins, 1)          # input last bit from GB
+    push(noblock)         # push the received value from ISR to RX FIFO
+    irq(rel(0))           # set interrupt
+    wrap()
+
+class GBPacket():
+    def __init__(self):
+        self.command = 0
+        self.compression_flag = 0
+        self.data_length = 0
+        self.data = bytearray(0x280)
+        # self.data = np.zeros(0x280, dtype=np.uint8)
+        self.checksum = 0
+        self.calc_checksum = 0
+
+
+class GBLink:
+    def __init__(self):
+        self.bytes_received = 0
+        # CLOCK PIN 5, ENTERED MANUALLY IN PROGRAM ABOVE
+        self.sm = rp2.StateMachine(
+            0, gb_link, 
+            in_base=Pin(2),
+            out_base=Pin(3),
+            set_base=Pin(25),
+            sideset_base=Pin(25),
+            freq = int(1e6)
+        )
+        self.state = STATE_IDLE
+        self.remaining_bytes = 0
+        self.packets = [GBPacket() for _ in range(12)]
+        self.packet_idx = 0
+        self.current_packet = self.packets[self.packet_idx]
+        self.got_packet = False
+        self.rx_byte = 0
+        self.tx_byte = 0
+        self.status = 0
+        self.packet_wait_start = 0
+        self.packet_end = 0
+        self.packet_start = 0
+        self.fake_print_ticks = 0
+    
+    def startup(self):
+        self.sm.irq(self.gb_interrupt)
+        self.sm.active(1)
+        self.sm.put(0x00)
+        self.run()
+        self.sm.active(0)
+    
+    def run(self):
+        while True:
+            self.packet_wait_start = utime.ticks_us()
+            while not self.got_packet:
+                pass
+            self.packet_end = utime.ticks_us()
+            self.handle_packet()
+            self.got_packet = False
+
+    def old_gb_interrupt(self, mach):
+        bite = mach.get()
+        self.bytes_received += 1
+        if self.bytes_received % 2:
+            mach.put(0x40)
+        else:
+            mach.put(0x81)
+        print(f'Received byte {bite:08x}')
+
+    def gb_interrupt(self, mach):
+        self.rx_byte = mach.get() # & 0xFF
+        # print(f'IRQ Received byte {self.rx_byte:02x}')
+        # print(f'Had sent {self.tx_byte:02x}')
+        self.process_byte()
+            
+    def process_byte(self):
+        self.tx_byte = 0x00
+
+        if self.state == STATE_IDLE:
+            self.tx_byte = 0x00
+            if self.rx_byte == 0x88:
+                self.state = STATE_MAGICBYTES_PARTIAL
+                self.packet_start = utime.ticks_us()
+
+        elif self.state == STATE_MAGICBYTES_PARTIAL:
+            if self.rx_byte == 0x33:
+                self.state = STATE_HEADER
+                self.remaining_bytes = 4
+            else:
+                self.state = STATE_IDLE
+
+        elif self.state == STATE_HEADER:
+            self.remaining_bytes -= 1
+            if self.remaining_bytes == 3:
+                self.current_packet.command = self.rx_byte
+                # self.current_packet.calc_checksum += self.rx_byte
+            elif self.remaining_bytes == 2:
+                self.current_packet.compression_flag = self.rx_byte
+                # self.current_packet.calc_checksum += self.rx_byte
+            elif self.remaining_bytes == 1:
+                self.current_packet.data_length = self.rx_byte
+                # self.current_packet.calc_checksum += self.rx_byte
+            else:
+                self.current_packet.data_length += self.rx_byte * 256
+                # self.current_packet.calc_checksum += self.rx_byte
+                if self.current_packet.data_length:
+                    self.state = STATE_PAYLOAD
+                    self.remaining_bytes = self.current_packet.data_length
+                else:
+                    self.state = STATE_CHECKSUM
+                    self.remaining_bytes = 2
+        
+        elif self.state == STATE_PAYLOAD:
+            idx = self.current_packet.data_length - self.remaining_bytes
+            self.remaining_bytes -= 1
+            self.current_packet.data[idx] = self.rx_byte
+            if self.remaining_bytes == 0:
+                self.state = STATE_CHECKSUM
+                self.remaining_bytes = 2
+        
+        elif self.state == STATE_CHECKSUM:
+            self.remaining_bytes -= 1
+            if self.remaining_bytes == 1:
+                self.current_packet.checksum = self.rx_byte
+            else:
+                self.current_packet.checksum += self.rx_byte * 256
+                self.state = STATE_RESPONSE_READY
+                self.tx_byte = 0x81 # first response byte
+        
+        elif self.state == STATE_RESPONSE_READY:
+            self.state = STATE_RESPONSE_PARTIAL
+            self.tx_byte = self.status
+
+        elif self.state == STATE_RESPONSE_PARTIAL:
+            self.state = STATE_IDLE
+            self.got_packet = True
+
+        self.sm.put(self.tx_byte)
+
+    def handle_packet(self):
+        packet_time = (self.packet_end-self.packet_start)
+        packet_spacing = (self.packet_start-self.packet_wait_start)
+        print(
+            f'Packet type: {self.current_packet.command}, '
+            f'Send time: {packet_time} us '
+            f'Time since last packet: {packet_spacing} us',
+            f'Print ticks: {self.fake_print_ticks}',
+        )
+        if self.current_packet.command == COMMAND_DATA:
+            self.packet_idx += 1
+            self.current_packet = self.packets[self.packet_idx]
+        elif self.current_packet.command == COMMAND_PRINT:
+            self.status = 0x06
+            self.fake_print_ticks = 20
+        elif (
+            self.current_packet.command == COMMAND_STATUS
+            and self.status == 0x06
+        ):
+            self.fake_print_ticks -= 1
+            if self.fake_print_ticks == 0:
+                self.status = 0x00
+                self.packet_idx = 0
+                self.current_packet = self.packets[self.packet_idx]
+        
+        
+
+
+link = GBLink()
+link.startup()
+
+
--- pio_test.py
+++ pio_test.py
@@ -1,188 +1,35 @@
-import time
 import rp2
-from machine import Pin
-from ulab import numpy as np
+import utime
+import uctypes
+
+@rp2.asm_pio(out_shiftdir=rp2.PIO.SHIFT_LEFT)
+def push_numbers(): 
+    set(x, 0)
+    label("loopie")
+    set(y, 0xA)
+    in_(y, 4)
+    set(y, 0xA)
+    in_(y, 4)
+    set(y, 0xB)
+    in_(y, 4)
+    set(y, 0xB)
+    in_(y, 4)
+    set(y, 0xC)
+    in_(y, 4)
+    set(y, 0xC)
+    in_(y, 4)
+    set(y, 0xD)
+    in_(y, 4)
+    set(y, 0xD)
+    in_(y, 4)
+    jmp(x_dec, "loopie")
+    push(noblock)
 
 
-STATE_IDLE = 0
-STATE_MAGICBYTES_PARTIAL = 1
-STATE_HEADER = 2
-STATE_PAYLOAD = 3
-STATE_DATASUM = 4
-STATE_DATASUM_DONE = 5
-STATE_RESPONSE_PARTIAL = 6
-STATE_TIMEOUT = 7
+sm = rp2.StateMachine(0, push_numbers, freq=int(1e6))
+sm.active(1)
 
-@rp2.asm_pio(
-    in_shiftdir=rp2.PIO.SHIFT_LEFT,
-    out_shiftdir=rp2.PIO.SHIFT_LEFT,
-    set_init=rp2.PIO.OUT_HIGH,
-    out_init=rp2.PIO.OUT_LOW,
-    sideset_init=rp2.PIO.OUT_LOW
-)
-def gb_link(): 
-    wrap_target() 
-    set(x, 6)             # set loop to run 6 + 1 times
-    wait(0, gpio, 5)      # wait for falling edge
-    set(pins, 0)          # turn off LED
-    pull(noblock)         # pull value from TX FIFO to OSR
-    out(null, 24)         # shift left by 24
-    out(pins, 1)          # out the MSB bit in OSR to GB
-    wait(1, gpio, 5)[2]   # wait for rising edge
-    label("bitloop")      
-    in_(pins, 1)          # input bit from GB to ISR
-    wait(0, gpio, 5)[2]   # wait for falling edge
-    out(pins, 1)          # output bit from OSR to GB
-    wait(1, gpio, 5)[1]   # wait for rising edge
-    jmp(x_dec, "bitloop") # loop through the rest of the bits
-    in_(pins, 1)          # input last bit from GB
-    push(noblock)         # push the received value from ISR to RX FIFO
-    irq(rel(0))           # set interrupt
-    wrap()
-
-class GBPacket():
-    def __init__(self):
-        self.command = 0
-        self.compression_flag = 0
-        self.len_lsb = 0
-        self.len_msb = 0
-        self.data = np.zeros(0x280, dtype=np.uint8)
-        self.checksum_lsb = 0
-        self.checksum_msb = 0
-    
-    @property
-    def data_length(self):
-        return self.len_lsb + self.len_msb * 256
-    
-    @property
-    def checksum(self):
-        return self.len_lsb + self.len_msb * 256
-
-class GBLink:
-    def __init__(self):
-        self.bytes_received = 0
-        # CLOCK PIN 5, ENTERED MANUALLY IN PROGRAM ABOVE
-        self.sm = rp2.StateMachine(
-            0, gb_link, 
-            in_base=Pin(2),
-            out_base=Pin(3),
-            set_base=Pin(25),
-            sideset_base=Pin(25),
-            freq = int(1e6)
-        )
-        self.state = STATE_IDLE
-        self.remaining_bytes = 0
-        self.current_packet = GBPacket()
-        self.got_packet = False
-        self.rx_byte = 0
-        self.tx_byte = 0
-        self.num_pages = 0
-        self.pages = np.zeros((20, 0x280), dtype=np.uint8)
-
-    def old_gb_interrupt(self, mach):
-        bite = mach.get() & 0xFF
-        self.bytes_received += 1
-        if self.bytes_received % 2:
-            mach.put(0x40)
-        else:
-            mach.put(0x81)
-        print(f'Received byte {bite:02x}')
-
-    def gb_interrupt(self, mach):
-        self.rx_byte = mach.get() & 0xFF
-        # print(f'IRQ Received byte {self.rx_byte:02x}')
-        # print(f'Had sent {self.tx_byte:02x}')
-        self.process_byte()
-    
-    def startup(self):
-        self.sm.irq(self.gb_interrupt)
-        self.sm.active(1)
-        self.sm.put(0x00)
-        self.run()
-    
-    def run(self):
-        while True:
-            while not self.got_packet:
-                pass
-            print(f'Packet type: {self.current_packet.command} Packet data: {1}')
-            if self.current_packet.command == 4:
-                pass
-                self.pages[self.num_pages,:] = self.current_packet.data
-                self.num_pages += 1
-            self.got_packet = False
-            
-    def process_byte(self):
-        done = False
-        # print(f'Received byte {self.rx_byte:02x}')
-        # print(f"State is {self.state}")
-        self.tx_byte = 0x00
-
-        if self.state == STATE_IDLE:
-            self.tx_byte = 0x00
-            if self.rx_byte == 0x88:
-                self.state = STATE_MAGICBYTES_PARTIAL
-                # print('pre here!')
-
-        elif self.state == STATE_MAGICBYTES_PARTIAL:
-            if self.rx_byte == 0x33:
-                self.state = STATE_HEADER
-                self.remaining_bytes = 4
-                # print('Received magic bytes!')
-            else:
-                self.state = STATE_IDLE
-
-        elif self.state == STATE_HEADER:
-            self.remaining_bytes -= 1
-            if self.remaining_bytes == 3:
-                self.current_packet.command = self.rx_byte
-            elif self.remaining_bytes == 2:
-                self.current_packet.compression_flag = self.rx_byte
-            elif self.remaining_bytes == 1:
-                self.current_packet.len_lsb = self.rx_byte
-            else:
-                self.current_packet.len_msb = self.rx_byte
-                # print('Received header!')
-                # print(f"Packet is type {self.current_packet.command}")
-                if self.current_packet.data_length:
-                    self.state = STATE_PAYLOAD
-                    self.remaining_bytes = self.current_packet.data_length
-                else:
-                    # print(f"Packet is dataless")
-                    self.state = STATE_DATASUM
-                    self.remaining_bytes = 2
-        
-        elif self.state == STATE_PAYLOAD:
-            idx = self.current_packet.data_length - self.remaining_bytes
-            self.remaining_bytes -= 1
-            self.current_packet.data[idx] = self.rx_byte
-            if self.remaining_bytes == 0:
-                # print('Done collecting data bytes')
-                self.state = STATE_DATASUM
-                self.remaining_bytes = 2
-        
-        elif self.state == STATE_DATASUM:
-            self.remaining_bytes -= 1
-            if self.remaining_bytes == 1:
-                self.current_packet.checksum_lsb = self.rx_byte
-            else:
-                self.current_packet.checksum_msb = self.rx_byte
-                # print("Received checksum!")
-                self.state = STATE_DATASUM_DONE
-                self.tx_byte = 0x81 # first response byte
-        
-        elif self.state == STATE_DATASUM_DONE:
-            self.state = STATE_RESPONSE_PARTIAL
-            self.tx_byte = 0x00 # should be actual status
-
-        elif self.state == STATE_RESPONSE_PARTIAL:
-            self.state = STATE_IDLE
-            self.got_packet = True
-
-        self.sm.put(self.tx_byte)
-        
-
-
-link = GBLink()
-link.startup()
-
-
+st = utime.ticks_us()
+a = sm.get()
+en = utime.ticks_us()
+print(f"Pio time: {en-st} us")
